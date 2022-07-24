702b34e8b1ae73f98b2ddab7a21740a8ebe5ff40
Omotola Olaoti <omotolaolaoti@gmail.com>
2022-07-24 23:02:31 +0100

    new commit

--- src/Nav.js
+++ src/Nav.js
@@ -1,21 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Nav.css";
 
 function Nav() {
+  const [navBg, setNavBg] = useState(false);
+  const handleScroll = () => {
+    if (window.scrollY > 50) {
+      setNavBg(true);
+    } else {
+      setNavBg(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  const [menuClick, setClick] = useState(false);
+  const checkClick = () => {
+    if (menuClick === true) {
+      setClick(false);
+    } else if (menuClick === false) {
+      setClick(true);
+    }
+  };
   return (
-    <header className="nav">
-      <div className="logo">
-        <span>GAND</span>isbr
+    <header className={`nav ${navBg && "navBg"}`}>
+      <div className="mobile-nav">
+        <div className="logo">
+          <span>GAND</span>isbr
+        </div>
+        <div
+          className={`menu ${menuClick ? "close" : "show"}`}
+          onClick={checkClick}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
       </div>
-      <div className="navLinks">
+      <div className={`navLinks ${menuClick ? "visible" : "hide"}`}>
         <ul>
+          <div className="mobile-header-bg"></div>
           <li className="navLinks__link">Top artists</li>
           <li className="navLinks__link">Marketplace</li>
           <li className="navLinks__link">Collections</li>
           <li className="navLinks__link">About us</li>
         </ul>
+        <div className="btn btn_fillpry">Get Started</div>
       </div>
-      <div className="btn btn_fillpry">Get Started</div>
     </header>
   );
 }
